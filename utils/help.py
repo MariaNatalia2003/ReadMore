1e93f7b20b439923b26ffd6e55705556feb6ad08
Maria Natália <cardim.natalia@gmail.com>
2024-10-31 15:58:04 -0300

    Atualização da lista de comando no help

--- utils/help.py
+++ utils/help.py
@@ -5,6 +5,8 @@ comandos_info = {
     "rm-pause": "Pausa a playlist que está tocando no momento.",
     "rm-continue": "Retoma a playlist que estava tocando.",
     "rm-stop": "Interrompe a playlist que está tocando no momento.",
+    "rm-skip": "Pula para a próxima música da lista de reprodução.",
+    "rm-welcomechannel": "Define um canal de texto para recebimento das mensagens de boas-vindas ao servidor.",
     "rm-startbook": "Salva a leitura atual com o nome do livro passado no comando.",
     "rm-checkdailyupdate": "Atualiza a meta diária de leitura para a quantidade de páginas passadas no comando.",
     "rm-checkdaily": "Verifica se você bateu sua meta de leitura e atualiza seu saldo.",
