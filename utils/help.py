022c31c758575f3b01f76fc8a4ea4aeddffe81e8
Maria Natália <cardim.natalia@gmail.com>
2024-11-30 22:20:15 -0300

    Alteração na lista do help

--- utils/help.py
+++ utils/help.py
@@ -11,7 +11,7 @@ comandos_info = {
     "rm-viewdailygoal": "Mostra a sua meta diária de leitura",
     "rm-checkdailyupdate": "Atualiza a meta diária de leitura para a quantidade de páginas passadas no comando.",
     "rm-checkdaily": "Verifica se você bateu sua meta de leitura e atualiza seu saldo.",
-    "rm-recommendation": "Recomendação de 5 livros aleatórios por gênero.",
+    "rm-recommendation": "Recomendação de 5 livros aleatórios por gênero (pesquise em inglês).",
     "rm-timer": "Cronômetro de tempo de leitura em minutos.",
     "rm-cash": "Mostra o seu saldo em moedas na economia do ReadMore.",
     "rm-finishread": "Finaliza sua leitura atual e atualiza a contagem de livros lidos.",
